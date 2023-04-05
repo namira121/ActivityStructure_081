fbf41ef74e9c14a537afa56052e764d648e4fa69
namira121 <127211008+namira121@users.noreply.github.com>
2023-04-05 11:28:23 +0700

    Membuat array dengan 3 elemen

--- Latihanstructure2/Latihanstructure2.cpp
+++ Latihanstructure2/Latihanstructure2.cpp
@@ -20,7 +20,7 @@ int main() {
 
 	for (int i = 0;i < 3;i++) {
 
-		cout << "Masukkan NIM : ";
+		cout << "Masukkan NIM : "; 
 		cin.getline(mhs[i].nim, 12);
 		cout << "Masukkan Umur : ";
 		cin >> mhs[i].umur;
