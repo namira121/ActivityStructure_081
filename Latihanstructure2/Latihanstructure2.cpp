d90cf4ef3a1a122b7c23e411c51314b7f33527d7
namira121 <127211008+namira121@users.noreply.github.com>
2023-04-05 11:27:34 +0700

    membuat struktur untuk Detail Alamat dengan char

---
+++ Latihanstructure2/Latihanstructure2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat
+{
+	char desa[20];
+	char kota[20];
+};
+
+struct Mahasiswa
+{
+	char nim[12];
+	char nama[20];
+	DetailAlamat alamat;
+	int umur;
+};
+
+int main() {
+	Mahasiswa mhs[3];
+
+	for (int i = 0;i < 3;i++) {
+
+		cout << "Masukkan NIM : ";
+		cin.getline(mhs[i].nim, 12);
+		cout << "Masukkan Umur : ";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+		cout << "Masukkan Nama : ";
+		cin.getline(mhs[i].nama, 20);
+		cout << "Masukkan Alamat : ";
+		cout << "\n\tMasukkan Desa : ";
+		cin.getline(mhs[i].alamat.desa, 20);
+		cout << "\n\tMasukkan Kota : ";
+		cin.getline(mhs[i].alamat.kota, 20);
+	}
+
+	for (int i = 0; i < 3; i++) {
+		cout << "\n\nNIM : " << mhs[i].nim;
+		cout << "\nUmur : " << mhs[i].umur;
+		cout << "\nNama : " << mhs[i].nama;
+		cout << "\nAlamat : ";
+		cout << "\n\tDesa : " << mhs[i].alamat.desa;
+		cout << "\n\tKota : " << mhs[i].alamat.kota;
+	}
+}
